a7503aff74962f4920669839bd7e315315823dfe
MHRiosBucarey <horaciriosnqn@gmail.com>
2023-08-16 20:32:44 -0300

    Integrador antes del cp

--- src/App.js
+++ src/App.js
@@ -1,33 +1,77 @@
-import './App.css';
-import Cards from './components/Cards.jsx';
-import Nav from './components/Nav';
-import { useState } from 'react';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Nav from "./components/Nav/Nav";
 import axios from "axios";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { removeFav } from "./redux/actions";
+import Home from "./views/Home/Home";
+import About from "./views/About/About";
+import Detail from "./views/Detail/Detail";
+import Login from "./views/Login/Login";
+import Favorites from "./views/Favorites/Favorites";
 
 function App() {
-     const[characters, setCharacters]=useState([]);
-     const onClose=(id) => {
-      const filtered= characters.filter((char)=>char.id!=id)
-      setCharacters(filtered);
-     }
-     function onSearch(characterID){
-         axios(`https://rickandmortyapi.com/api/character/${characterID}`).then(({data})=>{
-            if(data.name){
-               setCharacters([...characters,data])
-            }else{
-               window.alert(`No se encontro un personaje con el ID: ${characterID}`)
-            }
-         });
-      }
+  const [characters, setCharacters] = useState([]);
+  const { pathname } = useLocation(); // "/endpoint"
 
-      return (
-         <div className='App'>
-            <Nav onSearch= {onSearch} />
-            <Cards characters={characters} 
-            onClose={onClose} />
-            
-         </div>
-      );
-};
+  const navigate = useNavigate();
+  const [access, setAccess] = useState(false);
+  const EMAIL = "ejemplo@gmail.com";
+  const PASSWORD = "unaPassword";
+
+  const dispatch = useDispatch();
+
+  function login(dataUser) {
+    if (dataUser.password === PASSWORD && dataUser.email === EMAIL) {
+       setAccess(true);
+       navigate('/home');
+    } else {
+      alert("Usuario o clave incorrectos");
+    }
+ }
+
+ useEffect(() => {
+  !access && navigate('/');
+}, [access]);
+
+  function onSearch(id) {
+    axios(`https://rickandmortyapi.com/api/character/${id}`).then(
+      ({ data }) => {
+        if (data.name) {
+          setCharacters((oldChars) => [...oldChars, data]);
+        } else {
+          window.alert("¡No hay personajes con este ID!");
+        }
+      }
+    );
+  }
+
+  function onClose(id) {
+    // [{id},{id},{id},{id},{id}]
+    // fiterCharacters = [{id}, {id}, {id}]
+    const fiterCharacters = characters.filter(
+      (character) => character.id !== Number(id)
+    );
+    setCharacters(fiterCharacters);
+    dispatch(removeFav(id))
+  }
+
+  return (
+    <div className="App">
+      {pathname !== "/" && <Nav onSearch={onSearch} />}
+      <Routes>
+        <Route path="/" element={<Login login={login}/>} />
+        <Route
+          path="/home"
+          element={<Home characters={characters} onClose={onClose} />}
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/favorites" element={<Favorites onClose={onClose} />} />
+      </Routes>
+    </div>
+  );
+}
 
 export default App;
---
+++ src/views/About/About.jsx
@@ -0,0 +1,9 @@
+const About = () => (
+    <>
+      <h2>Acerca de mi</h2>
+      <img src="https://rickandmortyapi.com/api/character/avatar/3.jpeg" alt="" />
+    </>
+  );
+  
+  export default About;
+  
